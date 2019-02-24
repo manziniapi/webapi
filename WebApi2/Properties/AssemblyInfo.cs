0f5576b843c4e4003f94c00eac44cbd2675c356a
Gabriel Manzini <gabriel.manzini@apiconsulting.com.br>
2019-02-24 06:28:47 -0300

    commit inicial

---
+++ WebApi2/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As Informações Gerais sobre um assembly são controladas por meio do seguinte 
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("WebApi2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApi2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Configurar o ComVisible como false torna os tipos desse assembly invisíveis 
+// para componentes COM. Se for necessário acessar um tipo nesse assembly a partir do 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O seguinte GUID é para a ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("fe6950cd-0b2d-4724-9d4d-11c369e1f6c5")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// É possível especificar todos os valores ou utilizar como padrão os Números de Revisão e da Versão 
+// utilizando o "*" como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
